0842435a6dc839346121d73c1bdd34a2dfcd2a89
Kazuhiro Yamato <kz0817@gmail.com>
2014-12-15 04:17:49 +0000

    [DBTablesTest] Removed unused function: loadTestDBHostgroups().

--- server/test/DBTablesTest.h
+++ server/test/DBTablesTest.h
@@ -241,7 +241,6 @@ void loadTestDBArmPlugin(void);
 void loadTestDBTriggers(void);
 void loadTestDBEvents(void);
 void loadTestDBItems(void);
-//void loadTestDBHostgroups(void);
 void loadTestDBHostgroupElements(void);
 void loadTestDBServerStatus(void);
 
