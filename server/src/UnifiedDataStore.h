83af74649207131a269afc8c4eb6757a0c945a3c
Kazuhiro Yamato <kz0817@gmail.com>
2015-01-15 01:17:20 +0000

    [UnifiedDataStore] Remove an used deprecated method: getHostList().

--- server/src/UnifiedDataStore.h
+++ server/src/UnifiedDataStore.h
@@ -107,10 +107,6 @@ public:
 	*/
 
 	// Host and Hostgroup
-	void getHostList(HostInfoList &hostInfoList,
-	                 const HostsQueryOption &option)
-	  __attribute__ ((deprecated));
-
 	HatoholError getHostgroupInfoList(
 	  HostgroupInfoList &hostgroupInfoList,
 	  const HostgroupsQueryOption &option);
